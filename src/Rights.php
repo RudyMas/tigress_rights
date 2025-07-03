aba3b7bd62d990de735b98f0e02dfdb94d1c09a4
RudyMasGoNext <rudy.mas@go-next.be>
2025-07-03 12:03:05 +0200

    Added code to update the userRights so changed are instantly processed

--- src/Rights.php
+++ src/Rights.php
@@ -10,7 +10,7 @@ use Repository\SystemRightsRepo;
  * @author       Rudy Mas <rudy.mas@rudymas.be>
  * @copyright    2024-2025, Rudy Mas (http://rudymas.be/)
  * @license      https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version      2025.06.25.0
+ * @version      2025.07.03.0
  * @package      Tigress
  */
 class Rights
@@ -24,7 +24,7 @@ class Rights
      */
     public static function version(): string
     {
-        return '2025.06.25';
+        return '2025.07.03';
     }
 
     /**
@@ -35,6 +35,13 @@ class Rights
      */
     public function checkRights(string $action = 'access'): bool
     {
+        if (isset($_SESSION['user']['id'])) {
+            $_SESSION['userRights'] = RIGHTS->getSpecialRights($_SESSION['user']['id']);
+        } else {
+            // If Users class does not exist, assume no user is logged in
+            return false;
+        }
+
         $path = $_SERVER['REQUEST_URI'];
         $path = explode('?', $path)[0];
         if (strlen($path) > 1) {
